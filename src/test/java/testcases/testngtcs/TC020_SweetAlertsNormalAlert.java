65e7a9a3fe87a02d3be38951d40da08f08d959f8
evnraja32 <evnraja32@gmail.com>
2016-09-18 02:59:23 +0530

    Changes done after entering data into Test-case-log file

---
+++ src/test/java/testcases/testngtcs/TC020_SweetAlertsNormalAlert.java
@@ -0,0 +1,26 @@
+package testcases.testngtcs;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import util.AlertAction;
+import wrapper.TestNGWrapper;
+
+public class TC020_SweetAlertsNormalAlert extends TestNGWrapper{
+
+	@BeforeClass
+	public void setWebPageCofig(){
+		browser = "firefox";
+		url = "http://t4t5.github.io/sweetalert/";
+
+	}
+
+
+	@Test()
+	public void testCase() {
+		locateElementByXpath("//div[@class='showcase normal']/button");
+		clickOnElement();
+		switchToWindowAlert();
+		manageAlert(AlertAction.ACCEPT);
+	}
+}
