9a576847666e2e70e3667c42a26cd4922182c7ee
evnraja32 <evnraja32@gmail.com>
2016-09-19 20:56:16 +0530

    19 Sep Check in

---
+++ src/test/java/testcases/testngtcs/TC021_GridSampleIE.java
@@ -0,0 +1,35 @@
+package testcases.testngtcs;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class TC021_GridSampleIE {
+	
+	@Test(invocationCount = 3, threadPoolSize = 2)	
+	public void fte() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setBrowserName("internet explorer");
+
+		URL remoteAddress = new URL("http://192.168.1.104:4444/wd/hub");
+
+		RemoteWebDriver driver = new RemoteWebDriver(remoteAddress,desiredCapabilities);
+
+		driver.get("http://t4t5.github.io/sweetalert/");
+
+		driver.findElementByXPath("//div[@class='showcase normal']/button").click();
+
+		Alert a = driver.switchTo().alert();
+
+		a.accept();
+
+		driver.quit();
+
+
+
+	}
+}
---
+++ src/test/java/testcases/testngtcs/TC022_ScreenShotInRemoteIE.java
@@ -0,0 +1,43 @@
+package testcases.testngtcs;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+
+public class TC022_ScreenShotInRemoteIE {
+
+	@Test//(invocationCount = 3, threadPoolSize = 2)	
+	public void fte() throws WebDriverException, IOException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setBrowserName("internet explorer");
+
+		URL remoteAddress = new URL("http://192.168.1.104:4444/wd/hub");
+
+		RemoteWebDriver driver = new RemoteWebDriver(remoteAddress,desiredCapabilities);
+
+		driver.get("http://t4t5.github.io/sweetalert/");
+		new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='showcase normal']/button")));
+		long number = (long) Math.floor(Math.random() * 900000000L) + 10000000L;
+        try {
+			FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE) , new File("./output/reports/images/"+number+".jpg"));
+		} catch (WebDriverException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		driver.quit();
+
+	}
+}
