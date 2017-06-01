157dccb98c12b6d2ab857cb882d5326ee8ca04b2
evnraja32 <evnraja32@gmail.com>
2017-06-02 00:16:56 +0530

    Signed-off-by: evnraja32 <evnraja32@gmail.com>

--- src/main/java/oops/corejava/CountCharacterOccurence.java
+++ src/main/java/oops/corejava/CountCharacterOccurence.java
@@ -1,10 +1,12 @@
-class CountCharacterOccurence
-{
-    public static void main(String[] args)
-    {
-        String s = "Java is java again java again";
-        char c = 'a';
-        int count = s.length() - s.replace("a", "").length();
-        System.out.println("Number of occurances of 'a' in "+s+" = "+count);
-    }
-} 
+package oops.corejava;
+
+class CountCharacterOccurence {
+	public static void main(String[] args) {
+		String s = "Java is java again java again";
+		char c = 'a';
+
+		int count = s.length() - s.replace("a", "").length();
+
+		System.out.println("Number of occurances of 'a' in " + s + " = " + count);
+	}
+}					
