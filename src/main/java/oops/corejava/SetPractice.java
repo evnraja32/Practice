940e37433a8273eec82e1402bab57f23e4e295b2
evnraja32 <evnraja32@gmail.com>
2017-06-26 21:03:27 +0530

    Signed-off-by: evnraja32 <evnraja32@gmail.com>

---
+++ src/main/java/oops/corejava/SetPractice.java
@@ -0,0 +1,16 @@
+package oops.corejava;
+
+import java.util.HashSet;
+
+public class SetPractice {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void hashSet(){
+		HashSet<String> hashSet = new HashSet<String>();
+		
+		hashSet.add("");
+	}
+}
